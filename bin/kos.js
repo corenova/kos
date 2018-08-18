addef7ac9f865ad1f8a4ec4a7f932d7b1751f03d
Peter K. Lee <peter@corenova.com>
2018-08-17 17:35:26 -0700

    minor fixes to the new automata and persona entities

--- bin/kos.js
+++ bin/kos.js
@@ -6,8 +6,8 @@ const pkginfo = require('../package.json')
 
 const Kos = require('..')
 const NodeSchema    = require('../schema/node')
-const ConsoleSchema = require('../schema/console')
-const LogSchema     = require('../schema/log')
+// const ConsoleSchema = require('../schema/console')
+// const LogSchema     = require('../schema/log')
 
 function collect(val, keys) { keys.push(val); return keys }
 
@@ -21,8 +21,8 @@ program
   .parse(process.argv)
 
 Kos
-  .link(NodeSchema)
-  .link(ConsoleSchema)
-  .link(LogSchema)
-  .feed('node:process', process)
-  .feed('node:program', program)
+  .use(NodeSchema)
+  // .use(ConsoleSchema)
+  // .use(LogSchema)
+  .feed('nodejs:process', process)
+  .feed('nodejs:program', program)
--- schema/node.js
+++ schema/node.js
@@ -7,76 +7,74 @@ module.exports = require('./node.yang').bind({
   'feature(url)': function() { return require('url') },
   'feature(net)': function() { return require('net') },
 
-  runtime: {
-    // self-initialize when part of kos layers (usually kos directly)
-    initialize(process) { 
-      const parent = this.get('parent')
-      this.send('node:resolve', ...parent.depends)
-    },
+  // self-initialize when part of kos layers (usually kos directly)
+  initialize(process) { 
+    const parent = this.get('parent')
+    this.send('node:resolve', ...parent.depends)
+  },
 
-    execute(program) {
-      const { args=[], file=[], silent=false, verbose=0 } = program
+  execute(program) {
+    const { args=[], file=[], silent=false, verbose=0 } = program
 
-      // unless silent, setup logging
-      silent || this.send('node:log', { level: verbose })
+    // unless silent, setup logging
+    silent || this.send('node:log', { level: verbose })
 
-      // immediate processing of 'load' tokens first
-      this.sendImmediate('node:load', ...args)
-      this.send('node:read', ...file)
-    },
+    // immediate processing of 'load' tokens first
+    this.sendImmediate('node:load', ...args)
+    this.send('node:read', ...file)
+  },
 
-    load(name) {
-      const path = this.use('node:path')
-      const search = [ 
-        path.resolve(name),
-        path.resolve('schema', name),
-        path.resolve(__dirname, name),
-        name
-      ]
-      let schema
-      let location
-      for (location of search) {
-        try { schema = require(location); break }
-        catch (e) { 
-          if (e.code !== 'MODULE_NOT_FOUND') 
-            this.throw(e)
-        }
+  load(name) {
+    const path = this.use('node:path')
+    const search = [ 
+      path.resolve(name),
+      path.resolve('schema', name),
+      path.resolve(__dirname, name),
+      name
+    ]
+    let schema
+    let location
+    for (location of search) {
+      try { schema = require(location); break }
+      catch (e) { 
+        if (e.code !== 'MODULE_NOT_FOUND') 
+          this.throw(e)
       }
-      if (!schema) 
-        this.throw(`unable to locate schema "${name}" from ${search}`)
-      
-      if (!(schema instanceof Yang))
-        this.throw(`unable to load incompatible schema "${name}" from ${location}`)
-
-      this.send('kos:schema', schema)
-      this.send('node:resolve', ...schema.depends)
-    },
-
-    resolve(dep) {
-      const regex = /^module\/(.+)$/
-      let match = regex.exec(dep,'$1')
-      if (match) this.send('require', match[1])
-    },
-
-    require(opts) {
-      if (typeof opts === 'string') opts = { name: opts }
-      let { name, path } = opts
-      try {
-        const m = this.get(name) || require(path || name)
-        this.has(name) || this.set(name, m)
-        this.send('module/'+name, m)
-      } catch (e) {
-        e.target = name
-        this.error(e)
-      }
-    },
-
-    read(filename) {
-      const fs = this.get('module/fs')
-      const kson = fs.createReadStream(filename)
-      kson.on('error', this.error.bind(this))
-      kson.pipe(this.io, { end: false })
     }
+    if (!schema) 
+      this.throw(`unable to locate schema "${name}" from ${search}`)
+    
+    if (!(schema instanceof Yang))
+      this.throw(`unable to load incompatible schema "${name}" from ${location}`)
+
+    this.send('kos:schema', schema)
+    this.send('node:resolve', ...schema.depends)
+  },
+
+  resolve(dep) {
+    const regex = /^module\/(.+)$/
+    let match = regex.exec(dep,'$1')
+    if (match) this.send('require', match[1])
+  },
+
+  require(opts) {
+    if (typeof opts === 'string') opts = { name: opts }
+    let { name, path } = opts
+    try {
+      const m = this.get(name) || require(path || name)
+      this.has(name) || this.set(name, m)
+      this.send('module/'+name, m)
+    } catch (e) {
+      e.target = name
+      this.error(e)
+    }
+  },
+
+  read(filename) {
+    const fs = this.get('module/fs')
+    const kson = fs.createReadStream(filename)
+    kson.on('error', this.error.bind(this))
+    kson.pipe(this.io, { end: false })
   }
 })
 

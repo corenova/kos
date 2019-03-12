522d1a00f03dd53fc282a70963734590f798d78b
Peter K. Lee <peter@corenova.com>
2019-03-12 12:05:02 -0700

    allow kinetic-react to propagate state based on this.merge and this.set

--- schemas/kinetic-react-js.js
+++ schemas/kinetic-react-js.js
@@ -17,7 +17,7 @@ module.exports = require('./kinetic-react-js.yang').bind({
       }
       const { props, state, setState } = target
 
-      this.save(state); // update initial state
+      this.merge(state); // update initial state
 
       // override target to compute 'state' from this
       Object.defineProperty(target, 'state', {
@@ -47,7 +47,7 @@ module.exports = require('./kinetic-react-js.yang').bind({
       target.trigger = (topic, data) => {
         this.send('react:trigger', { topic, data })
       }
-      this.on('save', obj => this.send('react:state', obj))
+      this.on('update', prop => prop.changed && this.send('react:state', prop.change))
 
       props && this.send('react:props', props)
       setState && this.send('react:setter', setState.bind(target))

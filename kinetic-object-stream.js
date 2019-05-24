f76a7be5ab34cd33fd439b177e558777ca06f26d
Peter K. Lee <peter@corenova.com>
2019-05-23 17:20:06 -0700

    update to latest yang-js

--- kinetic-object-stream.js
+++ kinetic-object-stream.js
@@ -167,10 +167,9 @@ module.exports = require('./kinetic-object-stream.yang').bind({
       let from = this.lookup('kos:persona', this.tag);
       if (!from)
         throw this.error(`unable to resolve ${this.tag} persona`);
-      for (let n of from.nodes) {
+      for (const n of from.nodes) {
         if (n.kind === 'kos:reaction') {
-          // should we allow replace???
-          this.parent.merge(n.clone(true), { replace: true });
+          this.parent.merge(n.clone({ relative: false }));
         } else {
           this.parent.update(n.clone());
         }

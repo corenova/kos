42df620f6ea490b5c54cb479b5842617646805a0
Peter K. Lee <peter@corenova.com>
2019-06-04 12:58:26 -0700

    allow kos:reaction extends to replace duplicate functions

--- kinetic-object-stream.js
+++ kinetic-object-stream.js
@@ -169,7 +169,7 @@ module.exports = require('./kinetic-object-stream.yang').bind({
         throw this.error(`unable to resolve ${this.tag} persona`);
       for (const n of from.nodes) {
         if (n.kind === 'kos:reaction') {
-          this.parent.merge(n.clone({ relative: false }));
+          this.parent.merge(n.clone({ relative: false }, { replace: true }));
         } else {
           this.parent.update(n.clone());
         }
